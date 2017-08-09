d096ceb9a5b5d14785ea4b1f0a34be46594bf060
bulbafront <pavelexotic@gmail.com>
2017-08-10 02:10:07 +0300

    initial to-do functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { initialToDo } from './constants/initialToDo.js';
+import ToDoList from './containers/ToDoList.jsx';
+import { Grid, Row, Col } from 'react-bootstrap';
+import _ from 'lodash';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDoItems: initialToDo,
+      toDoItemsDone: [],
+    };
+  }
+
+  doneToDo = (label, isDone) => {
+    const removeArrayName = isDone ? 'toDoItemsDone' : 'toDoItems';
+    const addArrayName = !isDone ? 'toDoItemsDone' : 'toDoItems';
+    const removeArray = [].concat(this.state[removeArrayName]);
+    const addArray = [].concat(this.state[addArrayName], _.remove(removeArray, { label }));
+    this.setState({ [addArrayName]: addArray, [removeArrayName]: removeArray });
+  };
+
+
+  deleteToDo = (label, isDone) => {
+    const removeArrayName = isDone ? 'toDoItemsDone' : 'toDoItems';
+    const removeArray = [].concat(this.state[removeArrayName]);
+    _.remove(removeArray, { label });
+    this.setState({ [removeArrayName]: removeArray });
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +39,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>New Pet Project</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Grid>
+          <Row>
+            <h3>To Do List</h3>
+            <Col md={6} sm={12}>
+              <h4>Need To Do</h4>
+              <ToDoList toDoItems={this.state.toDoItems} doneToDo={this.doneToDo} deleteToDo={this.deleteToDo} />
+            </Col>
+            <Col md={6} sm={12}>
+              <h4>Done</h4>
+              <ToDoList toDoItems={this.state.toDoItemsDone} doneToDo={this.doneToDo} deleteToDo={this.deleteToDo} isDone />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
