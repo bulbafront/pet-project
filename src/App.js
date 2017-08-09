a4adcb2c8b1f1f37426e7ef4930604a88c0846d9
bulbafront <pavelexotic@gmail.com>
2017-08-10 02:41:21 +0300

    add new to do action added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { initialToDo } from './constants/initialToDo.js';
 import ToDoList from './containers/ToDoList.jsx';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, FormGroup, ControlLabel, FormControl, Form, Button } from 'react-bootstrap';
 import _ from 'lodash';
 
 class App extends Component {
@@ -14,7 +14,26 @@ class App extends Component {
       toDoItems: initialToDo,
       toDoItemsDone: [],
     };
+    this.labelInput = false;
+    this.subjectInput = false;
   }
+
+  resetFields = () => {
+    if (this.subjectInput && this.labelInput) {
+      this.subjectInput.value = '';
+      this.labelInput.value = '';
+    }
+  };
+
+  addNewToDo = () => {
+    const label = this.labelInput ? this.labelInput.value : false;
+    const subject = this.subjectInput ? this.subjectInput.value : false;
+    if (label && subject) {
+      const toDoItems = [].concat(this.state.toDoItems, [{ label, subject }]);
+      this.setState({ toDoItems }, this.resetFields)
+    }
+  };
+
 
   doneToDo = (label, isDone) => {
     const removeArrayName = isDone ? 'toDoItemsDone' : 'toDoItems';
@@ -50,6 +69,25 @@ class App extends Component {
             <Col md={6} sm={12}>
               <h4>Done</h4>
               <ToDoList toDoItems={this.state.toDoItemsDone} doneToDo={this.doneToDo} deleteToDo={this.deleteToDo} isDone />
+            </Col>
+          </Row>
+          <Row>
+            <h3>Add New To Do</h3>
+            <Col mdOffset={3} sm={12} md={6}>
+              <Form >
+                <FormGroup>
+                  <ControlLabel>Label</ControlLabel>
+                  {' '}
+                  <FormControl inputRef={ref => { this.labelInput = ref; }} type="text" placeholder="Label" />
+                </FormGroup>
+                <FormGroup >
+                  <ControlLabel>Subject</ControlLabel>
+                  <FormControl inputRef={ref => { this.subjectInput = ref; }} componentClass="textarea" placeholder="Subject" />
+                </FormGroup>
+                <Button onClick={this.addNewToDo}>
+                  Add To Do
+                </Button>
+              </Form>
             </Col>
           </Row>
         </Grid>
